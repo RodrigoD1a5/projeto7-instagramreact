c6159ea98f50af877200078f6e70003429dd9ed6
RodrigoD1a5 <rodrigobastos.diass@gmail.com>
2022-11-21 08:48:37 -0300

    Funcionalidade editar nome de usuario implementada

--- src/componentes/Usuario.js
+++ src/componentes/Usuario.js
@@ -1,12 +1,26 @@
+import React from "react"
+
 export default function Usuario() {
+    const [nome , setNome]= React.useState("Catana")
+
+    function nomeUsuario(){
+        const resposta = prompt("Qual o seu nome?")
+        if (!resposta){
+            alert("Digite seu nome corretamente")
+            setNome(nome)
+        }
+        else{
+            setNome(resposta)
+        }
+    }
     return (
         <div class="usuario">
             <img src="assets/img/catanacomics.svg" />
             <div class="texto">
                 <strong>catanacomics</strong>
                 <span>
-                    Catana
-                    <ion-icon name="pencil"></ion-icon>
+                    {`${nome}`}
+                    <ion-icon name="pencil" onClick={nomeUsuario}></ion-icon>
                 </span>
             </div>
         </div>
